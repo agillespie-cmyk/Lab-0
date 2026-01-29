84497c96f77c50beabaced3c4efafc0a1355833e
agillespie-cmyk <agillespie@wesleyan.edu>
2026-01-29 15:33:21 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+Python 3.14.2 (v3.14.2:df793163d58, Dec  5 2025, 12:18:06) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> print('Hello World! My name is Alexa')
+Hello World! My name is Alexa
+>>> 8-2
+6
+>>> 100*3
+300
+>>> 50/10
+5.0
+>>> 3**2
+9
+>>> 6+2
+8
+>>> (4+2)*6-1
+35
+>>> type((4+2)*6-1)
+<class 'int'>
+>>> float((8+2)*6-1)
+59.0
